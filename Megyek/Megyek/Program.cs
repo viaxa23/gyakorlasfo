3bc8cf6384e789cc36baebb46801c83abe74ba1d
visnyei.adam <visnyei.adam@O16G14>
2020-01-13 09:53:12 +0100

    megyék

---
+++ Megyek/Megyek/Program.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Megyek
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Megye [] megyek = new Megye;
+            for (int i = 0; i < megyek.Length; i++)
+            {
+                Console.Write("Kérem a megye nevét:");
+                string nev = Console.ReadLine();
+                Console.Write("Kérem a megye területét:");
+                int terulet = Int32.Parse(Console.ReadLine());
+                Console.Write("Kérem a megye székhelyét:");
+                string megyeszekhely = Console.ReadLine();
+                Console.Write("Nyugat-Magyarországi-e:");
+                bool keletiE = Boolean.Parse(Console.ReadLine());
+                megyek[i] = new Megye(nev, terulet, megyeszekhely, keletiE);
+            }
+            for (int i = 0; i < megyek.Length; i++)
+            {
+                megyek[i].KiirAdatok();
+            }
+
+
+
+            NyugatMagyarorszagiMegye nyMegye = new NyugatMagyarorszagiMegye(nev, terulet, megyeszekhely, nyugatiE); 
+            nyMegye.KiirAdatok();
+
+            Console.ReadKey(true);
+        }
+    }
+
+    class Megye
+    {
+       public string Nev { get; set; }
+        public int Terulet {get; set;}
+        public string Megyeszekhely { get; set;}
+
+        public bool KeletMagyarorszagiE { get; set; }
+
+        public Megye(string nev, int terulet, string megyeSzekhely)
+        {
+            this.Nev = nev;
+            this.Terulet = terulet;
+            this.Megyeszekhely = megyeSzekhely;
+            
+
+        }
+
+        public Megye(string nev, int terulet, string megyeSzekhely, bool keletiE) : 
+        {
+            this.Nev = nev;
+            this.Terulet = terulet;
+            this.Megyeszekhely = megyeSzekhely;
+            this.KeletMagyarorszagiE = keletiE;
+
+        }
+        public void KiirAdatok()
+        {
+            if (this.KeletMagyarorszagiE)
+            {
+                Console.WriteLine($"A megye neve: {this.Terulet} és megyeszékhelye: {this.Megyeszekhely}.");
+            }
+            
+        }
+    }
+    class NyugatMagyarorszagiMegye : Megye
+    {
+        public bool NyugatMagyarorszagiE { get; set; }
+
+        public NyugatMagyarorszagiMegye(string nev, int terulet, string megyeSzekhely, bool nyugatiE) : base(nev, terulet, megyeSzekhely)
+        {
+            this.NyugatMagyarorszagiE = nyugatiE;
+        }
+
+        new public void KiirAdatok()
+        {
+            if (this.NyugatMagyarorszagiE)
+            {
+                Console.WriteLine($"A megye neve: {this.Nev} megye, terület, megyeszékhely");
+            }
+        }
+    }
+
+}
---
+++ osztalyok_1/osztalyok_1/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace osztalyok_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Ember ember = new Ember(181, 85, "1998.02.15");
+            Ember emberke = ember;
+            //emberke.LekerMagassag() = 145;
+            Console.WriteLine($"Az ember magassága: {ember.LekerMagassag()} cm");
+            Console.WriteLine($"Az ember testsúlya: {ember.LekerTestsuly()} kg");
+            Console.WriteLine($"Az ember születési éve: {ember.LekerSzuletesiEv()}");
+            Console.WriteLine($"Az ember magassága: {emberke.LekerMagassag()} cm");
+            Console.WriteLine($"Az ember testsúlya: {emberke.LekerTestsuly()} kg");
+            Console.WriteLine($"Az ember születési éve: {emberke.LekerSzuletesiEv()}");
+            Console.ReadKey(true);
+        }
+    }
+    class Ember
+    {
+       private int magassag;
+       private int testsuly;
+       private string szuletesiEv;
+
+        public Ember(int Magassag, int Testsuly, string SzuletesiEv)
+        {
+            this.magassag = Magassag;
+            this.testsuly = Testsuly;
+            this.szuletesiEv = SzuletesiEv;
+        }
+
+        public int LekerMagassag()
+        {
+            return this.magassag;
+        }
+
+        public int LekerTestsuly()
+        {
+            return this.testsuly;
+        }
+
+        public string LekerSzuletesiEv()
+        {
+            return this.szuletesiEv;
+        }
+    }
+}
